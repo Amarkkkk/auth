a5ce91a7b843de64f65e49fb4dc15abeb1fe77a0
Amarkkkk <159623242+Amarkkkk@users.noreply.github.com>
2026-01-31 13:00:19 +0800

    Update API URL to live backend

--- api-frontend/src/services/api.js
+++ api-frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "https://auth-xhde.onrender.com/api";
 
 //create axios instance
 const api = axios.create({
